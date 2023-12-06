5d5837245ae4b582c6a13f6ff8e48e669e41ac8d
OlhaBozhko <helgablume58@gmail.com>
2023-12-06 15:25:57 +0100

    first commit

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,14 @@ import './helpers/postDate';
 import scrollSmooth from './helpers/smooth-scroll.js';
 
 function main() {
+  $(document).ready(function(){
+    $('.about__slider').slick({
+      dots: true,
+      adaptiveHeight: true,
+      nextArrow: '<div class="next arrow"></div>',
+      prevArrow: '<div class="prev arrow"></div>',
+    });
+  });
   scrollSmooth();
 }
 
